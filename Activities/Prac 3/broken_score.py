73f3692444fb313c3658c99b5197b02144e1ef5e
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-08-26 17:31:04 +1000

    Refactor score status program to use functions and random

---
+++ Activities/Prac 3/broken_score.py
@@ -0,0 +1,50 @@
+"""
+Broken Score
+Program to determine score status
+Now featuring functions and random-ness !!
+"""
+import random
+
+THRESHOLD_HIGH = 100
+THRESHOLD_LOW = 0
+LOW_SCORE = 50
+HIGH_SCORE = 90
+
+
+def main():
+    """Determine score status"""
+    score = get_valid_score("Enter score: ", THRESHOLD_LOW, THRESHOLD_HIGH)
+    score_status = determine_score_status(score, HIGH_SCORE, LOW_SCORE)
+    print_score_status(score, score_status)
+    random_score = generate_random_score()
+    score_status = determine_score_status(random_score, HIGH_SCORE, LOW_SCORE)
+    print_score_status(random_score, score_status)
+
+
+def get_valid_score(prompt, low, high):
+    score = float(input(prompt))
+    while score < low or score > high:
+        print("Invalid score")
+        score = float(input(prompt))
+    return score
+
+
+def determine_score_status(score, high, low):
+    if score >= high:
+        score_status = "Excellent :D"
+    elif score >= low:
+        score_status = "Passable :)"
+    else:
+        score_status = "Bad :("
+    return score_status
+
+
+def print_score_status(score, status):
+    print("Your score of {} is {}".format(score, status))
+
+
+def generate_random_score():
+    return random.randint(THRESHOLD_LOW, THRESHOLD_HIGH)
+
+
+main()
