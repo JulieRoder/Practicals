e8583ba56af0a7cc0a455866cebbe22ea854baae
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-08-27 15:09:43 +1000

    Add function modifications

--- Activities/Prac 4/subject_reader.py
+++ Activities/Prac 4/subject_reader.py
@@ -6,12 +6,14 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Extract data and print details"""
     data = get_data()
-    print(data)
+    print_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    list_of_parts = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,7 +24,26 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        list_of_parts.append(parts)
     input_file.close()
+    return list_of_parts
+
+
+def get_longest_name(list_file):
+    """Get length of longest name"""
+    longest_name = 0
+    for parts in list_file:
+        if len(parts[1]) > longest_name:
+            longest_name = len(parts[1])
+    return longest_name
+
+
+def print_details(list_file):
+    """Printing details from list"""
+    longest_name_length = get_longest_name(list_file)
+    for parts in list_file:
+        print("{} is taught by {:<{}} and has {:>3} students".format(parts[0], parts[1], longest_name_length,
+                                                                     parts[2]))
 
 
 main()
