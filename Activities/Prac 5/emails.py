989ae977ef38669c1a40eed8404ec4f983a0490c
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-09-03 15:46:28 +1000

    Final working code

--- Activities/Prac 5/emails.py
+++ Activities/Prac 5/emails.py
@@ -9,12 +9,25 @@ def main():
     user_email = input("Email: ").lower()
     while user_email != "":
         if user_email not in emails_and_names:
-            user_name = input("Name: ").title()
+            user_name = get_name(user_email)
             emails_and_names = {user_email: user_name}
         else:
             print("Email already entered. Please enter a new email.")
         user_email = input("Email: ").lower()
-    print(emails_and_names)
+    for key in emails_and_names:
+        print("{} ({})".format(emails_and_names[key], key))
+
+
+def get_name(email):
+    """Get name from email."""
+    index = email.find("@")
+    user_name = email[:index]
+    name_pieces = user_name.split(".")
+    full_name = " ".join(name_pieces).title()
+    confirmation = input("Is your name {}? (Y/n) ".format(full_name)).lower()
+    if confirmation == "n" or confirmation == "no":
+        full_name = input("Name: ")
+    return full_name
 
 
 main()
