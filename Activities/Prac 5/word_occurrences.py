0835bc68035e0e2abcbbb377eafed1073864965a
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-09-03 15:09:47 +1000

    Add functions

--- Activities/Prac 5/word_occurrences.py
+++ Activities/Prac 5/word_occurrences.py
@@ -3,15 +3,20 @@ CP1404 Practical 5
 Word Occurrences program
 """
 
-longest_word = 0
-user_text = input("Text: ").lower()
-words = user_text.split(" ")
-words.sort()
-word_count_dict = {}
-for word in words:
-    if len(word) > longest_word:
-        longest_word = len(word)
-    word_count_dict[word] = word_count_dict.get(word, 0) + 1
 
-for key in word_count_dict:
-    print("{:{}} = {}".format(key, longest_word, word_count_dict[key]))
+def main():
+    longest_word = 0
+    user_text = input("Text: ").lower()
+    words = user_text.split(" ")
+    words.sort()
+    word_count_dict = {}
+    for word in words:
+        if len(word) > longest_word:
+            longest_word = len(word)
+        word_count_dict[word] = word_count_dict.get(word, 0) + 1
+
+    for key in word_count_dict:
+        print("{:{}} = {}".format(key, longest_word, word_count_dict[key]))
+
+
+main()
