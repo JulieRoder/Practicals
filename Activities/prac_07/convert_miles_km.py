b5cfafa936c7c53d3cb04169cdb54201b9382155
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-09-24 15:57:04 +1000

    finished prac exercise

--- Activities/prac_07/convert_miles_km.py
+++ Activities/prac_07/convert_miles_km.py
@@ -13,7 +13,6 @@ class MilesToKilometreConverterApp(App):
     """Kivy app to convert miles into kilometres."""
     CONVERSION_RATE = 1.60934
     message = StringProperty()
-    number = float()
 
     def build(self):
         """Build the App from 'convert_miles_km.kv' file."""
@@ -22,14 +21,27 @@ class MilesToKilometreConverterApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_convert(self, miles):
+    def handle_convert(self):
         """Calculate the conversion from miles to kilometres."""
+        miles = self.valid_number()
         kilometres = miles * self.CONVERSION_RATE
         self.root.ids.output_label.text = str(kilometres)
 
-    def handle_increment(self, number, increment):
+    def handle_increment(self, increment):
+        """Increase input_number by given increment."""
+        number = self.valid_number()
         new_number = number + increment
         self.root.ids.input_number.text = str(new_number)
+
+    def valid_number(self):
+        """Returns a valid number or 0.0 ."""
+        try:
+            number = float(self.root.ids.input_number.text)
+            if number < 0:
+                return 0.0
+            return number
+        except ValueError:
+            return 0.0
 
 
 MilesToKilometreConverterApp().run()
