49bb528a2d9f85bd85a51c5c1da778feb66a8fbd
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-09-24 14:51:43 +1000

    Basic App build

---
+++ Activities/prac_07/convert_miles_km.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Practicals
+Miles to Kilometres Converter program
+Student name: Julie-Anne Roder
+"""
+from kivy.app import App
+from kivy.app import StringProperty
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class MilesToKilometreConverterApp(App):
+    """Kivy app to convert miles into kilometres."""
+    CONVERSION_RATE = 1.60934
+    message = StringProperty()
+
+    def build(self):
+        """Build the App from 'convert_miles_km.kv' file."""
+        self.title = 'Miles to Kilometre Converter'
+        Window.size = (300, 300)
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+MilesToKilometreConverterApp().run()
