b1ce8bd0dd19fc7125d13805b56ffcaa51add9a9
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-10-15 06:02:48 +1000

    add class variable

--- Activities/prac_08/taxi_test.py
+++ Activities/prac_08/taxi_test.py
@@ -6,7 +6,7 @@ Student name: Julie-Anne Roder
 
 from Activities.prac_08.taxi import Taxi
 
-prius_1 = Taxi("Prius 1", 100, 1.23)
+prius_1 = Taxi("Prius 1", 100)
 prius_1.drive(40)
 print(prius_1)
 print("Total fare is ${:.2f}".format(prius_1.get_fare()))
