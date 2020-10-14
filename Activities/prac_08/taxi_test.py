ee4613e5cd27f47921fe3524ddfef0fb083c0802
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-10-15 05:58:17 +1000

    working test program

---
+++ Activities/prac_08/taxi_test.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Practicals
+Taxi Test program
+Student name: Julie-Anne Roder
+"""
+
+from Activities.prac_08.taxi import Taxi
+
+prius_1 = Taxi("Prius 1", 100, 1.23)
+prius_1.drive(40)
+print(prius_1)
+print("Total fare is ${:.2f}".format(prius_1.get_fare()))
+prius_1.start_fare()
+prius_1.drive(100)
+print(prius_1)
+print("Total fare is ${:.2f}".format(prius_1.get_fare()))
