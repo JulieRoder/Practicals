16ba968c51145fcbc50deb1b48f9df649e4d9004
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-10-22 09:38:52 +1000

    finished exercise

--- Activities/prac_09/sort_files_1.py
+++ Activities/prac_09/sort_files_1.py
@@ -4,15 +4,20 @@ Sorting files 1
 Student name: Julie-Anne Roder
 """
 import os
+import shutil
 
 
 def main():
     """Sort files."""
+    os.chdir('FilesToSort')
+    file_types = []
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        for filename in filenames:
+            index = filename.find(".")
+            if filename[index+1:] not in file_types:
+                file_types.append(filename[index+1:])
+                os.mkdir(filename[index+1:])
+            shutil.move(filename, filename[index+1:])
 
 
-def tests():
-    """Test functions."""
-
-
-# main()
-tests()
+main()
