be8444fbede3def254a92604e1486629b67d8865
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-10-22 09:13:27 +1000

    initial commit

---
+++ Activities/prac_09/sort_files_1.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practicals
+Sorting files 1
+Student name: Julie-Anne Roder
+"""
+import os
+
+
+def main():
+    """Sort files."""
+
+
+def tests():
+    """Test functions."""
+
+
+# main()
+tests()
