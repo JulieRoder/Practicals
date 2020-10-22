7c541c19c7414d583d1446ee36044d2cf3b7a9c7
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-10-22 10:38:33 +1000

    finished exercise

--- Activities/prac_09/sort_files_2.py
+++ Activities/prac_09/sort_files_2.py
@@ -10,6 +10,27 @@ import shutil
 def main():
     """Sort files."""
     os.chdir('FilesToSort')
+    categories = []
+    file_location = {}
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        for filename in filenames:
+            index = filename.find(".")
+            file_extension = filename[index+1:]
+            if file_extension not in file_location:
+                category = get_sort_category(file_extension)
+                file_location[file_extension] = category
+                if category not in categories:
+                    categories.append(category)
+                    try:
+                        os.mkdir(category)
+                    except FileExistsError:
+                        pass
+            shutil.move(filename, file_location[file_extension])
+
+
+def get_sort_category(filetype):
+    category = input("What category would you like to sort {} files into? ".format(filetype))
+    return category
 
 
 main()
