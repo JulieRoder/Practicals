cc786dd5a78e4a8e3d6ea0062d187fb566dd9b72
Julie-Anne Roder <julieanne.roder@my.jcu.edu.au>
2020-11-05 09:14:26 +1000

    add get page

--- Activities/prac_10/wiki.py
+++ Activities/prac_10/wiki.py
@@ -13,8 +13,17 @@ def main():
     """
     search_string = input("Search wikipedia for: ")
     while search_string != "":
+        get_page(search_string)
         get_summary(search_string)
         search_string = input("Search wikipedia for: ")
+
+
+def get_page(string):
+    try:
+        search_page = wikipedia.page(string)
+        print(search_page.title, search_page.url)
+    except wikipedia.exceptions.PageError:
+        print("'{}' does not match any pages. Try another query!".format(string))
 
 
 def get_summary(string):
@@ -27,8 +36,10 @@ def get_summary(string):
 
 def run_tests():
     search_string = 'Barack'
+    get_page(search_string)
     get_summary(search_string)
-    search_string = 'Monty'
+    search_string = 'Monty'  # changes to monkey, not sure why?
+    get_page(search_string)
     get_summary(search_string)
 
 
